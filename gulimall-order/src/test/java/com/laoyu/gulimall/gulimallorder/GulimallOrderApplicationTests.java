1ae71571fbd316cd8f0cdca1960306fa4a43bebc
laoyu <953596700@qq.com>
2021-12-17 23:04:48 +0800

    maven configuration

---
+++ gulimall-order/src/test/java/com/laoyu/gulimall/gulimallorder/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laoyu.gulimall.gulimallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
